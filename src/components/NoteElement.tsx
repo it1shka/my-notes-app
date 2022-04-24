547979e7896117b734a06a0cabbb69d15daf3fb8
Tikhon Belousov <58363010+it1shka@users.noreply.github.com>
2022-04-24 23:48:19 +0300

    upd

--- src/components/NoteElement.tsx
+++ src/components/NoteElement.tsx
@@ -2,29 +2,37 @@ import { useEffect, useRef } from "react"
 import styled from "styled-components"
 import { Note } from "../store/slices/notes"
 
+const formatStamp = (n: number) => {
+  return new Date(n).toLocaleDateString('en-US')
+}
+
 const NoteElement = ({title, createdAt}: Note) => {
   return (
     <NoteContainer>
       <p>{title}</p>
-      <small>{createdAt}</small>
+      <small>{formatStamp(createdAt)}</small>
     </NoteContainer>
   )
 }
 
 const NoteContainer = styled.div<{color?: string}>`
-  
+  font-size: 1.5em;
   padding: 0.5em 1em;
   border-bottom: 1px solid ${({color}) => color ?? 'darkgreen'};
   background-color: white;
   color: ${({color}) => color ?? 'darkgreen'};
-
+  cursor: pointer;
+  
   transition: 0.1s all 0s;
   &:hover {
-    font-style: italic;
-    cursor: pointer;
     background-color: ${({color}) => color ?? 'darkgreen'};
     color: white;
   }
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
 `
 
 export default NoteElement
